7dad9e7016835f1303503377d655af6de25693d4
gisehweb <gisehbc@gmail.com>
2022-04-09 13:47:40 -0300

    creacion de tabla

---
+++ src/basic/migrations/m220409_160347_crear_table_usuarios.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220409_160347_crear_table_usuarios
+ */
+class m220409_160347_crear_table_usuarios extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('usuario', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull(),
+            'nombre' => $this->text(),
+            'apellido' => $this->text(),
+            'edad' => $this->integer(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('usuario');
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m220409_160347_crear_table_usuarios cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
